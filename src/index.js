f8576f6c7d766b1e159c3915b0674656e3894f70
RTCWDF\chinnam <harish.chinnam@realtech.com>
2022-10-12 12:33:59 +0200

    Rendering users list in the chat room

--- src/index.js
+++ src/index.js
@@ -20,20 +20,21 @@ io.on('connection', (socket)=>{
 
     socket.on('join', (options, callback)=>{
         const {error, user} = addUser({id:socket.id, ...options})
-        console.log(user)
         if(error){
             return callback(error)
         }
         socket.join(user.room)
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
         callback()
     })
 
     socket.on('sendMessage', (message, callback)=>{
-        console.log(socket.id)
         const user = getUser(socket.id)
-        console.log(user)
         const filter = new Filter()
 
         if (filter.isProfane(message)){
@@ -51,6 +52,10 @@ io.on('connection', (socket)=>{
         const user = removeUser(socket.id)
         if(user){
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
     })
     
